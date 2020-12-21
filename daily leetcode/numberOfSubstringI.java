cf06d8dc42ca9c77daa72cbf8d412e57816deeb9
asayu <asayu2019@icloud.com>
2020-12-21 12:47:53 -0600

    12/21/2020

---
+++ daily leetcode/numberOfSubstringI.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0, i = 0, count = 0, n = nums.length;
+        for(int j = 0; j < n; j++){
+            if(nums[j] % 2 == 1){
+                k--;
+                count = 0;
+            }
+            while(k == 0){
+                k += nums[i++] & 1;
+                count++;
+            }
+            //for every new even after kth even, we need to add all the count of number before the first odd
+            res += count;
+        }
+        return res;
+    }
+}
